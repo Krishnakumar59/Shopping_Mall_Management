3acf67e0a46a8921b7cdba4bfd4c72e2502cc2c7
@Kumar <krishnakumar.1905059@srec.ac.in>
2023-06-14 12:48:59 +0530

    Initial commit

---
+++ src/test/java/com/singledev/shopping_mall_management/ShoppingMallManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singledev.shopping_mall_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingMallManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
